edf8401b34c90a9ed9c076aff80c5b1b7e0afb9d
Oliver Nake <oliver.nake@web.de>
2023-09-09 18:12:29 +0200

    Projektdateien hinzufügen.

---
+++ FirstAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Schreibe ein Programm, welches die Grundrechenarten für die Zahlen 10 und 5 enthält.
+            // Die Rechenaufgaben und Ergebnisse sollen auf der Konsole angezeigt werden.
+        }
+    }
+}
